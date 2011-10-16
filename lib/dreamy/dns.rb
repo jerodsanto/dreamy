7e1a3ff4a18b50836e7fa18997a67937aede8910
Tal Atlas <tatlas@giltcity.com>
2011-10-16 17:40:36 -0400

    Added function to update dns record

--- lib/dreamy/dns.rb
+++ lib/dreamy/dns.rb
@@ -2,9 +2,9 @@ module Dreamy
   class Dns
     include EasyClassMaker
 
-    attributes  :account_id, :comment, :editable, :record, :type, :value, :zone
+    attributes  :account_id, :comment, :editable, :record, :type, :value, :zone, :account
 
-    def self.new_from_xml(xml)
+    def self.new_from_xml(xml, account=nil)
       d = new
       d.account_id  = (xml).at('account_id').innerHTML.to_i
       d.comment     = (xml).at('comment').innerHTML
@@ -13,7 +13,16 @@ module Dreamy
       d.type        = (xml).at('type').innerHTML
       d.value       = (xml).at('value').innerHTML
       d.zone        = (xml).at('zone').innerHTML
+      d.account     = account
       d
     end
+
+    def update new_type, new_value
+      account.dns_remove record, type, value
+      account.dns_add    record, new_type, new_value
+
+      self
+    end
+
   end
 end
